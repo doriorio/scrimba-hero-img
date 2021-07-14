211155a4cd2c51f5030a7179d4d88443473e0f9b
Dori C <itsdori@gmail.com>
2021-07-13 19:14:46 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `<html>
+    <head>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;500&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        
+        <section class="hero">
+            <p class="gradient-border">Pro course</p> 
+            <h1 class="site-title">Become a<strong> professional photographer </strong>in six weeks</h1>
+            <button class="btn" type="button"><span>Sign up</span></button>
+            <p>Course starting soon</p>
+        </section>
+        
+    </body>
+</html>
+`;
